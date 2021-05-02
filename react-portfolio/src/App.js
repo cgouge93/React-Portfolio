e1832d3577990453b12b781cff022bdd1e7e0e72
Carly G <73192129+cgouge93@users.noreply.github.com>
2021-05-02 03:26:41 -0500

    initial commit

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,12 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+     <div className="sidebar"></div>
+     <div className="main-content"></div>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-portfolio/src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function HomePage() {
+    return (
+        <div>
+            <h1>Carly Gouge</h1>
+        </div>
+    )
+}
+
+export default HomePage
