bb2ab5e3a9117038bc689aea56567d4531003a4b
Carly G <73192129+cgouge93@users.noreply.github.com>
2021-05-02 03:53:41 -0500

    set up sidebar

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,9 +1,12 @@
 import "./App.scss";
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
     <div className="App">
-     <div className="sidebar"></div>
+     <div className="sidebar">
+       <Navbar />
+     </div>
      <div className="main-content"></div>
     </div>
   );
