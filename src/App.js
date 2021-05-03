714f6d06643ac2f082eca06285b67ac0f29bae9f
Carly G <73192129+cgouge93@users.noreply.github.com>
2021-05-03 14:49:08 -0500

    added navigation toggle

--- src/App.js
+++ src/App.js
@@ -5,12 +5,25 @@ import AboutPage from "./pages/AboutPage";
 import ContactPage from "./pages/ContactPage";
 import PortfolioPage from "./pages/PortfolioPage";
 import {Switch, Route} from "react-router-dom";
+import { useState } from "react";
+
+
 
 function App() {
+  const [navToggle, setNavToggle] = useState(false);
+
+  const navClick = () =>{
+    setNavToggle(!navToggle)
+  }
   return (
     <div className="App">
-     <div className="sidebar">
+     <div className={`sidebar ${navToggle ? 'nav-toggle': ''}`}>
        <Navbar />
+     </div>
+     <div className="nav-btn" onClick={navClick}>
+       <div className="lines-1"></div>
+       <div className="lines-2"></div>
+       <div className="lines-3"></div>
      </div>
      <div className="main-content">
        <div className="content">
